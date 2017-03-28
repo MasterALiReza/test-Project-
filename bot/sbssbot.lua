efbc5d90eed146a29795070fbc3bc6eb086fcccb
AliReza Nazari <AliRezanazari998@gmail.com>
2017-03-28 11:29:55 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {144658773,114934518,94746365},--Sudo users
+    sudo_users = {97921329,97921329,97921329},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
@@ -343,9 +343,9 @@ _________________________________________
 _________________________________________
 _________________________________________
 _________________________________________
-ليست سودوها :@Cia_00_07
-@sina7sk
-@amirho3in
+مدیریت :@Mast3rAliReza
+https://t.me/joinchat/AAAAAEAtZIeCbGT6JX0ZRg
+@Mast3rAliReza
 ]]
   }
   serialize_to_file(config, './data/config.lua')
